066493b81c448276c08a73c19f7ada487e4358ee
DEVitor0 <vitormoreira6940@gmail.com>
2024-07-23 12:52:43 -0300

    criação de todo o ambiente para trabalho

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    main: ['./src/assets/js/script.js']
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'public/assets/js')
+  },
+  module: {
+    rules: [{
+      exclude: /node_modules/,
+      test: /\.js$/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/env']
+        }
+      }
+    }]
+  },
+  devtool: 'source-map'
+};
